873179ab59156db4ecef372f051e327e59aaf228
Sanjana Sekhar <ssekhar1@jhu.edu>
2020-09-20 03:04:40 -0400

    Update find_hit_position.py

--- code/find_hit_position.py
+++ code/find_hit_position.py
@@ -14,6 +14,7 @@ from keras.layers.core import Dropout
 from keras.layers.core import Dense
 from keras.layers import Flatten
 from keras.layers import Input
+from keras.layers import Concatenate
 import tensorflow as tf
 from keras.callbacks import ModelCheckpoint
 import matplotlib
@@ -27,28 +28,34 @@ import numpy as np
 
 # Load data
 f = h5py.File('h5_files/train_subset.hdf5', 'r')
-input_train = f['train_hits'][...]
+pix_train = f['train_hits'][...]
+cosx_train = f['cosx'][...]
+cosy_train = f['cosy'][...]
+cosz_train = f['cosz'][...]
 label_train = f['x'][...]
 f.close()
 f = h5py.File('h5_files/test_subset.hdf5', 'r')
-input_test = f['test_hits'][...]
+pix_test = f['test_hits'][...]
+cosx_test = f['cosx'][...]
+cosy_test = f['cosy'][...]
+cosz_test = f['cosz'][...]
 label_test = f['x'][...]
 f.close()
 
-#print(np.reshape(input_train[2],(13,21)))
-#print(label_train.shape)
+#print(np.reshape(pix_train[2],(13,21)))
+print(cosx_train.shape)
 '''
-input_train = np.zeros((n_train,13,21,1))
+pix_train = np.zeros((n_train,13,21,1))
 label_train = np.zeros((n_train,1))
 #shuffle the training arrays -> FIND MORE EFFICIENT WAY TO DO THIS
 #create subset
 j=0
 for i in range(0,41):
-	input_train[1000*i:1000*(i+1)]=input_train_1[j:1000+j]
+	pix_train[1000*i:1000*(i+1)]=pix_train_1[j:1000+j]
 	label_train[1000*i:1000*(i+1)]=label_train_1[j:1000+j]
 	j+=30000
 
-input_test = input_test_1[0:n_test]
+pix_test = pix_test_1[0:n_test]
 label_test = label_test_1[0:n_test]
 '''
 # Model configuration
@@ -78,8 +85,9 @@ x = BatchNormalization(axis=-1)(x)
 x = MaxPooling2D(pool_size=(2, 2))(x)
 x = Dropout(0.25)(x)
 x = Flatten()(x)
-x_out_cnn = Dense(128)(x)
-x = Activation("relu")(x_out_cnn)
+angles = Input(shape=(3))
+x = Dense(128)(x)
+x = Activation("relu")(x)
 x = BatchNormalization()(x)
 x = Dropout(0.5)(x)
 x = Dense(1)(x)
@@ -103,17 +111,16 @@ callbacks = [
 ]
 
 # Fit data to model
-history = model.fit(input_train, label_train,
+history = model.fit(pix_train, label_train,
             batch_size=batch_size,
             epochs=n_epochs,
             callbacks=callbacks,
             verbose=verbosity,
             validation_split=validation_split)
 
-print(x_out_cnn)
 ''''
 # Generate generalization metrics
-results = model.predict(input_test, batch_size=batch_size)
+results = model.predict(pix_test, batch_size=batch_size)
 #print("test loss, test acc:", results)
 print results[:20]
 residuals = results-label_test
