98de4cfdd81863ed6f942cb7bc90970c4cc9446d
Sanjana <sanjanasekhar39@gmail.com>
2020-11-16 00:31:17 -0600

    fixed plots

--- code/plotter.py
+++ code/plotter.py
@@ -47,20 +47,18 @@ def plot_by_clustersize(residuals,clustersize,label,img_ext):
 	clustersize = np.asarray(clustersize)
 	max_size = int(np.amax(clustersize))
 	sigma_per_size = np.zeros((max_size,1))
-	for i in range(max_size):
-		indices = np.argwhere(clustersize==i)
-		
+	for i in range(1,max_size):
+		indices = np.argwhere(clustersize==i)[:,0]
+		print(i,':',indices.shape)
 		if(len(indices)==0):
 			sigma_per_size[i]=0.
 		else:
-			residuals_per_size = residuals[indices[0]]
+			residuals_per_size = residuals[indices]
 			sigma_per_size[i] = np.std(residuals_per_size)
-			print(i)
-			print(len(residuals_per_size))
 			plt.hist(residuals_per_size, bins=np.arange(-60,60,0.25), histtype='step',linewidth=2, label=r'$\vartriangle$'+label)
 			plt.xlabel(r'$\mu m$')
 			plt.title('residuals in %s for clustersize = %i, %s = %0.2f'%(label,i,r'$\sigma$',sigma_per_size[i]))
-			#plt.savefig("plots/res_%s_csize%i_%s.png"%(label,i,img_ext))
+			plt.savefig("plots/res_%s_csize%i_%s.png"%(label,i,img_ext))
 			plt.close()
 
 	x = np.linspace(1, max_size, max_size)
@@ -68,6 +66,6 @@ def plot_by_clustersize(residuals,clustersize,label,img_ext):
 	plt.xlabel('clustersize in %s'%(label))
 	plt.ylabel('resolution in %s'%(label))
 	plt.title('resolution vs clustersize in %s'%(label))
-	#plt.savefig("plots/resvssize_%s_%s"%(label,img_ext))
+	plt.savefig("plots/resvssize_%s_%s"%(label,img_ext))
 	plt.close()
 
