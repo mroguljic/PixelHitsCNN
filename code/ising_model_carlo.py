c04f6ae0a8499a440249ebe2ea43280de4aa2f0c
Sanjana <sanjanasekhar39@gmail.com>
2020-12-08 20:28:39 -0600

    hmmm

--- code/ising_model_carlo.py
+++ code/ising_model_carlo.py
@@ -16,47 +16,59 @@ def monte_carlo_ising(Q,N,kT,lattice):
 
 	ising = np.zeros((Q,N,N))
 	mag = np.zeros((Q,1))
-	E_i,E_f,accept = 0,0,0
+	accept = 0
 
-	for index in range(0,Q):
+	for index in range(0,2):
+		E_i,E_f=0,0
 		#generate a random no i and j for index of spin to be flipped
 		i,j,r = rng.randint(0,N), rng.randint(0,N), rng.uniform(0,1)
-		test_lattice = lattice
+		print('===============================')
+		print(i,j,r)
+		test_lattice = np.copy(lattice)
 		#flip
 		test_lattice[i,j] = -test_lattice[i,j]
-
+		
 		#Compute energy for both configs
-
+		#numpy is modifying the original too
 		#check right
 		if(j!=N-1):
-			E_i+=lattice[i,j]*lattice[i,j+1]
-			E_f+=test_lattice[i,j]*test_lattice[i,j+1]
+			print('right')
+			E_i+=-(lattice[i,j]*lattice[i,j+1])
+			E_f+=-(test_lattice[i,j]*test_lattice[i,j+1])
 		#check left
 		if(j!=0):
-			E_i+=lattice[i,j]*lattice[i,j-1]
-			E_f+=test_lattice[i,j]*test_lattice[i,j-1]
+			print('left')
+			E_i+=-(lattice[i,j]*lattice[i,j-1])
+			E_f+=-(test_lattice[i,j]*test_lattice[i,j-1])
 		#check top 
 		if(i!=0):
-			E_i+=lattice[i,j]*lattice[i-1,j]
-			E_f+=test_lattice[i,j]*test_lattice[i-1,j]
+			print('top')
+			E_i+=-(lattice[i,j]*lattice[i-1,j])
+			E_f+=-(test_lattice[i,j]*test_lattice[i-1,j])
 		#check bottom
 		if(i!=N-1):
-			E_i+=lattice[i,j]*lattice[i+1,j]
-			E_f+=test_lattice[i,j]*test_lattice[i+1,j]
+			print('bottom')
+			E_i+=-(lattice[i,j]*lattice[i+1,j])
+			E_f+=-(test_lattice[i,j]*test_lattice[i+1,j])
 
 		#make the choice 
+		print('E_i = ',E_i)
+		print(lattice)
+		print('E_f = ',E_f)
+		print(test_lattice)
 		delE = E_f - E_i 
-		if(delE < 0 or (delE > 0 and r < exp(-delE/kT))):
-			lattice = test_lattice
-			ising[accept] = test_lattice
+		print('delE = ',delE)
+		if(delE < 0 or (delE >= 0 and r < np.exp(-delE/kT))):
+			lattice = np.copy(test_lattice)
+			ising[accept] = lattice
 			#find magnetization
 			#N_plus = np.sum(lattice.clip(0,1))
 			#N_minus = N*N - N_plus
 			#M = (N_plus - N_minus)/(N*N)
 			mag[accept] = (2*np.sum(lattice.clip(0,1))-(N*N))/(N*N)
 			accept+=1
-
-	return ising[:-accept],mag[:-accept]
+	print('accept = ',accept)
+	return ising[:accept],mag[:accept]
 
 N = 10
 Q = 10000
