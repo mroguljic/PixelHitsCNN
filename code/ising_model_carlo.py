bde23e1ab05943cac5a7b7f527014df3f3f2a7da
Sanjana Sekhar <ssekhar1@jhu.edu>
2020-12-08 21:39:24 -0500

    Update ising_model_carlo.py

--- code/ising_model_carlo.py
+++ code/ising_model_carlo.py
@@ -18,12 +18,12 @@ def monte_carlo_ising(Q,N,kT,lattice):
 	mag = np.zeros((Q,1))
 	accept = 0
 
-	for index in range(0,2):
+	for index in range(0,Q):
 		E_i,E_f=0,0
 		#generate a random no i and j for index of spin to be flipped
 		i,j,r = rng.randint(0,N), rng.randint(0,N), rng.uniform(0,1)
-		print('===============================')
-		print(i,j,r)
+		##print('===============================')
+		##print(i,j,r)
 		test_lattice = np.copy(lattice)
 		#flip
 		test_lattice[i,j] = -test_lattice[i,j]
@@ -32,32 +32,32 @@ def monte_carlo_ising(Q,N,kT,lattice):
 		#numpy is modifying the original too
 		#check right
 		if(j!=N-1):
-			print('right')
+			#print('right')
 			E_i+=-(lattice[i,j]*lattice[i,j+1])
 			E_f+=-(test_lattice[i,j]*test_lattice[i,j+1])
 		#check left
 		if(j!=0):
-			print('left')
+			#print('left')
 			E_i+=-(lattice[i,j]*lattice[i,j-1])
 			E_f+=-(test_lattice[i,j]*test_lattice[i,j-1])
 		#check top 
 		if(i!=0):
-			print('top')
+			#print('top')
 			E_i+=-(lattice[i,j]*lattice[i-1,j])
 			E_f+=-(test_lattice[i,j]*test_lattice[i-1,j])
 		#check bottom
 		if(i!=N-1):
-			print('bottom')
+			#print('bottom')
 			E_i+=-(lattice[i,j]*lattice[i+1,j])
 			E_f+=-(test_lattice[i,j]*test_lattice[i+1,j])
 
 		#make the choice 
-		print('E_i = ',E_i)
-		print(lattice)
-		print('E_f = ',E_f)
-		print(test_lattice)
+		#print('E_i = ',E_i)
+		#print(lattice)
+		#print('E_f = ',E_f)
+		#print(test_lattice)
 		delE = E_f - E_i 
-		print('delE = ',delE)
+		#print('delE = ',delE)
 		if(delE < 0 or (delE >= 0 and r < np.exp(-delE/kT))):
 			lattice = np.copy(test_lattice)
 			ising[accept] = lattice
@@ -68,7 +68,7 @@ def monte_carlo_ising(Q,N,kT,lattice):
 			mag[accept] = (2*np.sum(lattice.clip(0,1))-(N*N))/(N*N)
 			accept+=1
 	print('accept = ',accept)
-	return ising[:accept],mag[:accept]
+	return ising[:accept+1],mag[:accept+1]
 
 N = 10
 Q = 10000
@@ -77,7 +77,7 @@ kT = 1.5
 
 #Start off with a random config
 lattice = rng.choice([1, -1], size=(N, N))
-print(lattice)
+#print(lattice)
 ising_config, mag = monte_carlo_ising(Q,N,kT,lattice)
 
 plt.hist(mag, bins=np.arange(-2,2,0.1), histtype='step', density=True,linewidth=2)
