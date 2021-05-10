16bbed5201dbc395961ac5e212bc3bdb244f1f16
Sanjana Sekhar <ssekhar1@jhu.edu>
2021-05-10 16:20:47 -0400

    Update inferCNN_cfg.py

--- test/inferCNN_cfg.py
+++ test/inferCNN_cfg.py
@@ -7,18 +7,6 @@ from FWCore.ParameterSet.VarParsing import VarParsing
 from Configuration.AlCa.GlobalTag import GlobalTag
 from Configuration.Eras.Era_Run2_2018_cff import Run2_2018
 
-# -- Conditions
-process.load("Configuration.StandardSequences.MagneticField_38T_cff")
-process.load('Configuration.StandardSequences.GeometryRecoDB_cff')
-process.load("RecoVertex.BeamSpotProducer.BeamSpot_cfi")
-process.load('Configuration.StandardSequences.RawToDigi_Data_cff')
-process.load('Configuration.StandardSequences.L1Reco_cff')
-process.load('Configuration.StandardSequences.Reconstruction_Data_cff')
-process.load('Configuration.StandardSequences.RawToDigi_Data_cff')
-process.load("Geometry.TrackerGeometryBuilder.trackerGeometry_cfi")
-process.load("Geometry.TrackerNumberingBuilder.trackerNumberingGeometry_cfi")
-process.load("Configuration.StandardSequences.MagneticField_cff")
-process.load("Configuration.StandardSequences.Geometry_cff")
 
 graph_ext = "1dcnn_p1_apr12"
 
@@ -41,6 +29,19 @@ datadir = "/uscms_data/d3/ssekhar/CMSSW_11_1_2/src/TrackerStuff/PixelHitsCNN/dat
 
 # define the process to run
 process = cms.Process("TEST")
+# -- Conditions
+process.load("Configuration.StandardSequences.MagneticField_38T_cff")
+process.load('Configuration.StandardSequences.GeometryRecoDB_cff')
+process.load("RecoVertex.BeamSpotProducer.BeamSpot_cfi")
+process.load('Configuration.StandardSequences.RawToDigi_Data_cff')
+process.load('Configuration.StandardSequences.L1Reco_cff')
+process.load('Configuration.StandardSequences.Reconstruction_Data_cff')
+process.load('Configuration.StandardSequences.RawToDigi_Data_cff')
+process.load("Geometry.TrackerGeometryBuilder.trackerGeometry_cfi")
+process.load("Geometry.TrackerNumberingBuilder.trackerNumberingGeometry_cfi")
+process.load("Configuration.StandardSequences.MagneticField_cff")
+process.load("Configuration.StandardSequences.Geometry_cff")
+
 
 # minimal configuration
 process.load("FWCore.MessageService.MessageLogger_cfi")
@@ -48,18 +49,18 @@ process.load("FWCore.MessageService.MessageLogger_cfi")
 process.MessageLogger.cerr.FwkReport.reportEvery = 1
 process.maxEvents = cms.untracked.PSet(input=cms.untracked.int32(-1))
 process.source = cms.Source("PoolSource",
-    fileNames=cms.untracked.vstring("root://cms-xrd-global.cern.ch//store/data/Run2018C/SingleMuon/RAW/v1/000/320/065/00000/8C070B38-338E-E811-A4D1-FA163E781D28.root")
+  fileNames=cms.untracked.vstring("root://cms-xrd-global.cern.ch//store/data/Run2018C/SingleMuon/RAW/v1/000/320/065/00000/8C070B38-338E-E811-A4D1-FA163E781D28.root")
 
 # process options
 process.options = cms.untracked.PSet(
-    allowUnscheduled=cms.untracked.bool(True),
-    wantSummary=cms.untracked.bool(True),
-)
+  allowUnscheduled=cms.untracked.bool(True),
+  wantSummary=cms.untracked.bool(True),
+  )
 
 # to get the conditions you need a GT
 process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
 process.GlobalTag = GlobalTag(process.GlobalTag, '112X_dataRun2_v7', '')
-                            
+
 process.load("RecoTracker.TransientTrackingRecHit.TTRHBuilderWithTemplate_cfi")
 process.TTRHBuilderAngleAndTemplate.PixelCPE = cms.string("PixelCPEGeneric")
 
@@ -67,18 +68,18 @@ process.TTRHBuilderAngleAndTemplate.PixelCPE = cms.string("PixelCPEGeneric")
 #process.load("TrackerStuff.PixelHitsCNN.inferCNN_cfi
 # CLEANUP 
 process.inferCNN = cms.EDAnalyzer('InferCNN',
-     graphPath_x = cms.string(os.path.join(datadir, "graph_x_%s.pb"%(graph_ext))),
-     graphPath_y = cms.string(os.path.join(datadir, "graph_y_%s.pb"%(graph_ext))),
-     inputTensorName_x = cms.string("input_1"),
-     anglesTensorName_x = cms.string("input_2"),
-     inputTensorName_y = cms.string("input_3"),
-     anglesTensorName_y = cms.string("input_4"),
-     outputTensorName = cms.string("Identity"),
+ graphPath_x = cms.string(os.path.join(datadir, "graph_x_%s.pb"%(graph_ext))),
+ graphPath_y = cms.string(os.path.join(datadir, "graph_y_%s.pb"%(graph_ext))),
+ inputTensorName_x = cms.string("input_1"),
+ anglesTensorName_x = cms.string("input_2"),
+ inputTensorName_y = cms.string("input_3"),
+ anglesTensorName_y = cms.string("input_4"),
+ outputTensorName = cms.string("Identity"),
      #mightGet = cms.optional.untracked.vstring,
     # trackCollectionLabel = cms.untracked.InputTag('generalTracks'),
     # PrimaryVertexCollectionLabel = cms.untracked.InputTag('offlinePrimaryVertices'),
      #pixelRecHitLabel             = cms.untracked.InputTag('siPixelRecHits')
-   )
+     )
 
 
 # define what to run in the path
@@ -107,4 +108,4 @@ process.schedule = cms.Schedule(
   process.reconstruction_step,
   #process.TrackRefitter_step,
   process.pixelCPECNN_step
-)
+  )
