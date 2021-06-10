1d0ddaaa7eb0e7d5069a82c6a44196b999fa5794
Sanjana <sanjanasekhar39@gmail.com>
2021-06-10 04:13:33 -0500

    ??

--- plugins/Infer2DCNN.cpp
+++ plugins/Infer2DCNN.cpp
@@ -530,12 +530,12 @@ public:
 				tensorflow::run(session_, {{inputTensorName_,cluster_}, {anglesTensorName_,angles}}, {outputTensorName_x}, &output_x);
 				tensorflow::run(session_, {{inputTensorName_,cluster_}, {anglesTensorName_,angles}}, {outputTensorName_y}, &output_y);
 				// convert microns to cms
-				x_2dcnn[count] = -output_x[0].matrix<float>()(0,0);
-				y_2dcnn[count] = -output_y[0].matrix<float>()(0,0);
+				x_2dcnn[count] = output_x[0].matrix<float>()(0,0);
+				y_2dcnn[count] = output_y[0].matrix<float>()(0,0);
 				//printf("x = %f y = %f\n",x_2dcnn[count],y_2dcnn[count]);
 
-				x_2dcnn[count] = (x_2dcnn[count]+pixelsize_x*(mid_x))*micronsToCm;
-				y_2dcnn[count] = (y_2dcnn[count]+pixelsize_y*(mid_y))*micronsToCm;
+				x_2dcnn[count] = (x_2dcnn[count]-pixelsize_x*(mid_x))*micronsToCm;
+				y_2dcnn[count] = (y_2dcnn[count]-pixelsize_y*(mid_y))*micronsToCm;
 				// go back to module coordinate system
 				x_2dcnn[count]+=lp.x();
 				y_2dcnn[count]+=lp.y(); 
