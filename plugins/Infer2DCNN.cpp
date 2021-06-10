27ee92ddee3dfdb9b92004cc37b4433c2aff5eb4
Sanjana <sanjanasekhar39@gmail.com>
2021-06-10 05:08:20 -0500

    -0.5

--- plugins/Infer2DCNN.cpp
+++ plugins/Infer2DCNN.cpp
@@ -534,8 +534,8 @@ public:
 				y_2dcnn[count] = output_y[0].matrix<float>()(0,0);
 				//printf("x = %f y = %f\n",x_2dcnn[count],y_2dcnn[count]);
 
-				x_2dcnn[count] = (x_2dcnn[count]+pixelsize_x*(mid_x+0.5))*micronsToCm;
-				y_2dcnn[count] = (y_2dcnn[count]+pixelsize_y*(mid_y+0.5))*micronsToCm;
+				x_2dcnn[count] = (x_2dcnn[count]+pixelsize_x*(mid_x-0.5))*micronsToCm;
+				y_2dcnn[count] = (y_2dcnn[count]+pixelsize_y*(mid_y-0.5))*micronsToCm;
 				// go back to module coordinate system
 				x_2dcnn[count]+=lp.x();
 				y_2dcnn[count]+=lp.y(); 
