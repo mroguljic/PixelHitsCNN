00f992afabb461ae1ef778ae87b2097296984e52
Sanjana Sekhar <ssekhar1@jhu.edu>
2021-06-10 00:57:59 -0400

    modifying the centre by 1

--- plugins/Infer2DCNN.cpp
+++ plugins/Infer2DCNN.cpp
@@ -465,8 +465,8 @@ int clustersize_x = 0, clustersize_y = 0;
 					}
 				}
 				//printf("clustersize_x = %i, clustersize_y = %i\n",clustersize_x,clustersize_y);
-				int mid_x = int(clustersize_x/2)-1;
-				int mid_y = int(clustersize_y/2)-1;
+				int mid_x = int(clustersize_x/2);
+				int mid_y = int(clustersize_y/2);
 				int offset_x = 6 - mid_x;
 				int offset_y = 10 - mid_y;
 				//printf("offset_x = %i, offset_y = %i\n",offset_x,offset_y);
@@ -530,9 +530,9 @@ int clustersize_x = 0, clustersize_y = 0;
 				tensorflow::run(session_, {{inputTensorName_,cluster_}, {anglesTensorName_,angles}}, {outputTensorName_y}, &output_y);
 				// convert microns to cms
 				x_2dcnn[count] = output_x[0].matrix<float>()(0,0);
-				x_2dcnn[count] = (x_2dcnn[count]+pixelsize_x*(mid_x+0.5))*micronsToCm;
+				x_2dcnn[count] = (x_2dcnn[count]+pixelsize_x*(mid_x))*micronsToCm;
 				y_2dcnn[count] = output_y[0].matrix<float>()(0,0);
-				y_2dcnn[count] = (y_2dcnn[count]+pixelsize_y*(mid_y+0.5))*micronsToCm;
+				y_2dcnn[count] = (y_2dcnn[count]+pixelsize_y*(mid_y))*micronsToCm;
 				// go back to module coordinate system
 				x_2dcnn[count]+=lp.x();
 				y_2dcnn[count]+=lp.y(); 
